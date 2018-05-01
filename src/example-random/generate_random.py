62b4f5ce6dc4a37b11cd2aa09dc8175da461e5a6
Lukas Käll <lukas.kall@gmail.com>
2018-05-01 21:26:34 +0200

    sx

--- src/example-random/generate_random.py
+++ src/example-random/generate_random.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 
-original = pd.read_csv('Signatures.tsv', sep='\t')
+path = "~/git/binox/example/"
+
+original = pd.read_csv(path + 'Signatures.tsv', sep='\t')
 
 genes = np.unique(original.index)
 
@@ -10,7 +12,7 @@ random_signatures = pd.DataFrame()
 for i in np.arange(1,10001,1):
     sample = np.random.choice(len(genes),10, replace=False)
     sample_genes = genes[sample]
-    new_group = pd.DataFrame(np.transpose([sample_genes, np.repeat('group' + str(i),10)])) 
+    new_group = pd.DataFrame(np.transpose([sample_genes, np.repeat('group' + str(i),10)]))
     random_signatures = random_signatures.append(new_group)
 
 random_signatures.columns = ['#Node', 'GroupName']
@@ -19,11 +21,10 @@ random_signatures.set_index('#Node').to_csv('Random_Signatures.tsv', sep = '\t')
 
 #####
 
-pathways = pd.read_csv('Pathways.tsv', sep='\t', header=None)
+pathways = pd.read_csv(path + 'Pathways.tsv', sep='\t', header=None)
 
 pathway_name = 'GLYCOLYSIS_/_GLUCONEOGENESIS_-_HOMO_SAPIENS_(HUMAN)'
 
 one_pathway = pathways.where(pathways.iloc[:,1] == pathway_name).dropna()
 
 one_pathway.to_csv('Pathway_One.tsv', sep='\t', header=None, index=False)
-
