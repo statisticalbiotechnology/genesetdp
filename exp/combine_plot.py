bfba0c9f3ea65bd52b961f7719a18a961585fd84
Gustavo Jeuken <gjeuken@gmail.com>
2018-05-07 16:41:14 +0200

    new calibration plots

--- exp/combine_plot.py
+++ exp/combine_plot.py
@@ -3,30 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+Q = 30
 
-binox = pd.read_csv('example-random/BinoX_results.tsv', sep='\t')
+binox = pd.read_csv('example-random/BinoX_results_Q' + str(Q) + '.tsv', sep='\t')
 
 ###### only enriched
-binox.loc[binox['#6:relationType'] == '-', '#4:p.value'] = 1 - binox.loc[binox['#6:relationType'] == '-', '#4:p.value']
+binox.loc[:, 'enrichment'] = binox.loc[:, '#4:p.value']
+binox.loc[binox['#6:relationType'] == '-', 'enrichment'] = 1 - binox.loc[binox['#6:relationType'] == '-', '#4:p.value']
 ######
-
-
 
 binox = binox.set_index('#3:NameGroupB')
 
 binox.index = [x.lower() for x in binox.index]
 
-binox = binox['#4:p.value']
+binox_both = binox['#4:p.value']
+binox_enrich = binox['enrichment']
 
-genesetdp = pd.read_csv('example-random/genesetDP_results.tsv', sep='\t', index_col=0)
+genesetdp = pd.read_csv('example-random/genesetDP_results_Q' + str(Q) + '.tsv', sep='\t', index_col=0)
 genesetdp_sorted = genesetdp['p'].sort_values()
 genesetdp_sorted_mc = genesetdp['p_mc'].sort_values()
 genesetdp_ix = (np.arange(len(genesetdp_sorted.index))+0.5)/float(len(genesetdp_sorted))
 
-binox = binox.loc[genesetdp.index]
-binox.loc[np.isnan(binox)] = 1
-binox_sorted = binox.sort_values()
-binox_ix = (np.arange(len(binox_sorted.index))+0.5)/float(len(binox_sorted.index))
+binox_both = binox_both.loc[genesetdp.index]
+binox_enrich = binox_enrich.loc[genesetdp.index]
+binox_both.loc[np.isnan(binox_both)] = 1
+binox_enrich.loc[np.isnan(binox_enrich)] = 1
+
+binox_both_sorted = binox_both.sort_values()
+binox_enrich_sorted = binox_enrich.sort_values()
+
+binox_ix = (np.arange(len(binox_both_sorted.index))+0.5)/float(len(binox_both_sorted.index))
 
 x_y = pd.DataFrame(np.arange(0,1,0.001), index = np.arange(0,1,0.001))
 x_2y = pd.DataFrame(np.arange(0,1,0.001)*2, index = np.arange(0,1,0.001))
@@ -35,16 +41,17 @@ x_halfy = pd.DataFrame(np.arange(0,0.5,0.001), index = np.arange(0,0.5,0.001)*2)
 sns.set_style("ticks")
 sns.despine()
 
-
-plt.loglog(binox_ix, binox_sorted, '.')
+plt.rc('font', size=12)
+plt.loglog(binox_ix, binox_both_sorted, '.')
+plt.loglog(binox_ix, binox_enrich_sorted, '.')
 plt.loglog(genesetdp_ix, genesetdp_sorted,'.')
 plt.loglog(genesetdp_ix, genesetdp_sorted_mc,'.')
 plt.loglog(x_y, '--', color = 'r', alpha = 0.5)
 plt.loglog(x_2y, ':',color = 'b', alpha = 0.5)
 plt.loglog(x_halfy, ':',color = 'b', alpha = 0.5)
 
-plt.legend(['BinoX', 'GeneSetDP', 'GeneSetDP, monte carlo', 'y=x', 'y=0.5x,y=2x'])
 plt.ylabel('$p$ value')
 plt.xlabel('Normalized rank')
 
-plt.savefig('calibration.png')
+plt.savefig('calibration_Q' + str(Q) + '.png')
+plt.show()
---
+++ exp/combine_plot_multiple.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def one_plot(ax, Q):
+
+    binox = pd.read_csv('example-random/BinoX_results_Q' + str(Q) + '.tsv', sep='\t')
+
+    ###### only enriched
+    binox.loc[:, 'enrichment'] = binox.loc[:, '#4:p.value']
+    binox.loc[binox['#6:relationType'] == '-', 'enrichment'] = 1 - binox.loc[binox['#6:relationType'] == '-', '#4:p.value']
+    ######
+
+    binox = binox.set_index('#3:NameGroupB')
+
+    binox.index = [x.lower() for x in binox.index]
+
+    binox_both = binox['#4:p.value']
+    binox_enrich = binox['enrichment']
+
+    genesetdp = pd.read_csv('example-random/genesetDP_results_Q' + str(Q) + '.tsv', sep='\t', index_col=0)
+    genesetdp_sorted = genesetdp['p'].sort_values()
+    genesetdp_sorted_mc = genesetdp['p_mc'].sort_values()
+    genesetdp_ix = (np.arange(len(genesetdp_sorted.index))+0.5)/float(len(genesetdp_sorted))
+
+    binox_both = binox_both.loc[genesetdp.index]
+    binox_enrich = binox_enrich.loc[genesetdp.index]
+    binox_both.loc[np.isnan(binox_both)] = 1
+    binox_enrich.loc[np.isnan(binox_enrich)] = 1
+
+    binox_both_sorted = binox_both.sort_values()
+    binox_enrich_sorted = binox_enrich.sort_values()
+
+    binox_ix = (np.arange(len(binox_both_sorted.index))+0.5)/float(len(binox_both_sorted.index))
+
+    x_y = pd.DataFrame(np.arange(0,1,0.001), index = np.arange(0,1,0.001))
+    x_2y = pd.DataFrame(np.arange(0,1,0.001)*2, index = np.arange(0,1,0.001))
+    x_halfy = pd.DataFrame(np.arange(0,0.5,0.001), index = np.arange(0,0.5,0.001)*2)
+
+    sns.set_style("ticks")
+    sns.despine()
+
+    ax.loglog(binox_ix, binox_both_sorted, '.')
+    ax.loglog(binox_ix, binox_enrich_sorted, '.')
+    ax.loglog(genesetdp_ix, genesetdp_sorted,'.')
+    ax.loglog(genesetdp_ix, genesetdp_sorted_mc,'.')
+    ax.loglog(x_y, '--', color = 'r', alpha = 0.5)
+    ax.loglog(x_2y, ':',color = 'b', alpha = 0.5)
+    ax.loglog(x_halfy, ':',color = 'b', alpha = 0.5)
+
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True)
+
+one_plot(ax1, 10)
+one_plot(ax2, 20)
+one_plot(ax3, 30)
+
+ax1.set_title('Q = 10')
+ax2.set_title('Q = 30')
+ax3.set_title('Q = 30')
+
+ax1.set(ylabel='$p$ value', xlabel = 'Normalized rank')
+ax2.set(xlabel = 'Normalized rank')
+ax3.set(xlabel = 'Normalized rank')
+
+# for ax in [ax1,ax2,ax3]:
+#     ax.set_aspect('equal')
+
+lgd = fig.legend(['BinoX', 'BinoX (enriched)     ', 'GeneSetDP', 'GeneSetMC', 'y=x', 'y=0.5x,y=2x'], loc='center left', bbox_to_anchor=(0.85,0.5))
+
+fig.set_size_inches(10,3)
+fig.savefig('calibration_multiple.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+
+
+# plt.savefig('calibration_multiple.png')
+# plt.show()
