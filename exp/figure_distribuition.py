08b69d724e7cc9d942f83142a696670285403802
Gustavo Jeuken <gjeuken@gmail.com>
2018-05-07 11:08:52 +0200

    some results for Q10,20,30

---
+++ exp/figure_distribuition.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+import sys
+sys.path.insert(0, '../src')
+import generate_k as genk
+import genesetdp
+
+## example path
+path = "~/git/binox/example/"
+
+## read original patway information file
+pathways_file = 'Pathways.tsv'
+pathways = pd.read_csv(path + pathways_file, sep='\t', header=None)
+pathways.columns = ['gene','pathway']
+
+pathway_name = 'GLYCOLYSIS_/_GLUCONEOGENESIS_-_HOMO_SAPIENS_(HUMAN)'
+
+pathway_genes = pathways.where(pathways.pathway == pathway_name).dropna().gene.values
+
+k = genk.generate_k(pathway_genes)
+
+N = genesetdp.genesetdp(k,10)
+
+####
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("ticks")
+sns.despine()
+
+plt.step(np.arange(len(N)), N, where='mid')
+plt.ylabel('$N(s)$')
+plt.xlabel('Score, $s$')
+plt.title('Q = 10')
+
+plt.savefig('score_distribuition_Q10.png')
+plt.show()
