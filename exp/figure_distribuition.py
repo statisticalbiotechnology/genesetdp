77545eea4e5fec44140b97eca6ed694a87b22b13
Gustavo Jeuken <gjeuken@gmail.com>
2018-05-08 10:46:05 +0200

    plots

--- exp/figure_distribuition.py
+++ exp/figure_distribuition.py
@@ -20,19 +20,37 @@ pathway_genes = pathways.where(pathways.pathway == pathway_name).dropna().gene.v
 
 k = genk.generate_k(pathway_genes)
 
-N = genesetdp.genesetdp(k,30)
+N10 = genesetdp.genesetdp(k,10)
+N20 = genesetdp.genesetdp(k,20)
+N30 = genesetdp.genesetdp(k,30)
 
 ####
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+
 sns.set_style("ticks")
 sns.despine()
 
-plt.step(np.arange(len(N)), N, where='mid')
-plt.ylabel('$N(s)$', fontsize=14)
-plt.xlabel('Score, $s$', fontsize=14)
+ax1.set_ylabel('Q = 10\n$N(s)$', fontsize=16)
+ax2.set_ylabel('Q = 20\n$N(s)$', fontsize=16)
+ax3.set_ylabel('Q = 30\n$N(s)$', fontsize=16)
+ax3.set_xlabel('Score, $s$', fontsize=16)
 
-plt.savefig('score_distribuition_Q30.png')
+for ax in [ax1,ax2,ax3]:
+    ax.xaxis.set_tick_params(labelsize=12)
+    ax.yaxis.set_tick_params(labelsize=12)
+
+
+ax1.step(np.arange(len(N10)), N10, where='mid')
+ax2.step(np.arange(len(N20)), N20, where='mid')
+ax3.step(np.arange(len(N30)), N30, where='mid')
+
+plt.tight_layout()
+plt.savefig('score_distribuition_multiple.png')
 plt.show()
+
+
+
